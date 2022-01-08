2802e03cd2c23951147fb5fe4568ad5c9727b9f4
Kathryn Hodgson <k3hdgsn@gmail.com>
2022-01-08 13:24:07 +0000

    clean up code

--- trees/src/Button.js
+++ trees/src/Button.js
@@ -3,13 +3,11 @@ import "./App.css";
 import { AiFillLike } from "react-icons/ai";
 
 export default function Icons(props) {
-  // like button & counter
   const [buttonLike, setButtonLike] = useState("rgb(128,128,128)");
   const [likeCounter, setLikeCounter] = useState(0);
 
   const changeLike = () => {
     if (buttonLike === "rgb(128,128,128)") {
-      console.log("here");
       setButtonLike("rgb(0,0,0)");
     }
     setLikeCounter(likeCounter + 1);
